4fec8fc138840122372e04d8247788187caf3934
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-02 15:47:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//your JS code here. If required.
+let submit = document.getElementById("submit");
+let player1 = document.getElementById("player-1");
+let player2 = document.getElementById("player-2");
