a3799031fec0b6db58d6fcb4b3a961db089bdbba
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-10 22:38:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let submit = document.getElementById("submit");
 let player1 = document.getElementById("player-1");
 let player2 = document.getElementById("player-2");
+let message = document.getElementById("message");
+let grid = document.getElementById("grid");
+
+grid.addEventListener("click", function(event){
+	if (event.target.tagName === "TD") {
+    event.target.textContent = "X"; // Put an "X" in the clicked cell
+  }
+})
